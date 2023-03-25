d0c2cbb58f6aaf6a5793934c044fe2a8ded6e642
JobaerAbdi <jobaerrahman757@gmail.com>
2023-03-25 22:12:29 +0600

    First Commit

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+            <div className='flex justify-between'>
+                <div className='w-4/5 bg-stone-400'>
+                    <h1>gdfgdfg</h1>
+                </div>
+                <div className='w-1/5 bg-orange-500'>
+                    <h1>dsdsfsf</h1>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Shop;
