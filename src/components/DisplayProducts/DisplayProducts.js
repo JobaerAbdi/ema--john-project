c85a8161813cf5b05990be9c9d0b34bf4941cb25
JobaerAbdi <jobaerrahman757@gmail.com>
2023-03-25 22:48:15 +0600

    Update

--- src/components/DisplayProducts/DisplayProducts.js
+++ src/components/DisplayProducts/DisplayProducts.js
@@ -11,7 +11,7 @@ const DisplayProducts = ({product}) => {
     <h2 className="card-title">{name}</h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+      <button className="btn btn-primary">Details</button>
     </div>
   </div>
 </div>
