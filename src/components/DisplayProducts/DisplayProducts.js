24854bfef9aba57463a329d925f162b046236382
JobaerAbdi <jobaerrahman757@gmail.com>
2023-03-25 22:36:51 +0600

    Second Commit

---
+++ src/components/DisplayProducts/DisplayProducts.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const DisplayProducts = ({product}) => {
+    const {img,name} = product;
+    console.log(product);
+    return (
+        <div>
+    <div className="card card-compact w-full h-full px-6 py-4 bg-base-100 shadow-xl">
+  <figure><img className='p-4' src={img} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">{name}</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default DisplayProducts;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import DisplayProducts from '../DisplayProducts/DisplayProducts';
 
 const Shop = () => {
+    const [products,setProducts] = useState([]);
+    // console.log(products);
+    useEffect(()=>{
+        fetch('fakeData.json')
+        .then(res => res.json())
+        .then(data => setProducts(data));
+    },[]);
     return (
         <div>
             <div className='flex justify-between'>
-                <div className='w-4/5 bg-stone-400'>
-                    <h1>gdfgdfg</h1>
+                <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-6 w-4/5 bg-stone-400'>
+                    {
+                        products.map(product=> <DisplayProducts
+                        key={product.id} product={product}>
+                        </DisplayProducts>)
+                    }
                 </div>
-                <div className='w-1/5 bg-orange-500'>
-                    <h1>dsdsfsf</h1>
+                <div className='w-1/5 bg-orange-500 mt-6'>
+                    <h1 className='font-bold text-3xl mt-4'>Order Summary :</h1>
                 </div>
             </div>
             
